b837807d12a9627fc78f8cfd255cf8341d7c9239
Owale128 <Christian.buaka@hotmail.com>
2024-09-02 21:18:08 +0200

    Added "Naruto" as initial state in query

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { AnimeContext } from "../context/AnimeContext";
 
 const Layout = () => {
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState('Naruto')
   const location = useLocation();
   const detailPage = location.pathname.startsWith('/animeDetails');
 
